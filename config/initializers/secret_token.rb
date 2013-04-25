59dfd0442647b133a1fb7980b7e78f11f375a5c3
ichabodcole <ichabodcole@gmail.com>
2013-04-25 10:00:25 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KixTdd::Application.config.secret_token = '0006f703e7add109f5acba27bf7457e2dcf24432c5175e04b88b9879ce65721f8300d76148a4ff5dee6c961225e9800000ca826d10f7620230b116b7a25211b9'
